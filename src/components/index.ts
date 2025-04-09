b49727f611a731f7524d04a74d4140ae85db75b7
Cash Myers <cmyers880@gmail.com>
2025-04-08 21:18:12 -0500

    fix: default exports correction

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,11 @@
 export { default as ActionButton } from "./ActionButton";
 export { default as Button } from "./Button";
+export { default as Checkbox } from "./Checkbox";
+export { default as ColorPicker } from "./ColorPicker";
 export { default as ConfirmDialog } from "./ConfirmDialog";
+export { default as DatePicker } from "./DatePicker";
 export { default as Dialog } from "./Dialog";
+export { default as RadioGroup } from "./RadioGroup";
+export { default as Select } from "./Select";
+export { default as Switch } from "./Switch";
 export { default as TextAreaAutosize } from "./TextAreaAutosize";
